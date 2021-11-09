baf5dde9a1c34fc22e62442ff644857b4acfa83f
DevHealthGroup <79181717+DevHealthGroup@users.noreply.github.com>
2021-11-08 21:50:15 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import app, db
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     db.create_all()
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=port)
     
